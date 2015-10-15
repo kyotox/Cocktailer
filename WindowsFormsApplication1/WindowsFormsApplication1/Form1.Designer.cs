506fcc1f2368d81632f3683fb5734a8382f5f62f
Dragos Chiotoroiu <dragos.costin93@gmail.com>
2015-10-16 01:39:51 +0200

    Load db

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newCocktailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCocktailToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(399, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newCocktailToolStripMenuItem
+            // 
+            this.newCocktailToolStripMenuItem.Name = "newCocktailToolStripMenuItem";
+            this.newCocktailToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.newCocktailToolStripMenuItem.Text = "New cocktail";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.Location = new System.Drawing.Point(288, 74);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(86, 17);
+            this.checkBox15.TabIndex = 2;
+            this.checkBox15.Text = "checkBox15";
+            this.checkBox15.UseVisualStyleBackColor = true;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(288, 39);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 261);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.checkBox15);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoadAll);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newCocktailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.Label labelName;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,83 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LoadAll(object sender, EventArgs e)
+        {
+            string[] available_ingredients;
+            available_ingredients = new string[10];
+
+            // Load available ingredients from file and cocktails database
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"available_ing.txt");
+
+
+                short ingNo = 0;
+                foreach (string line in lines)
+                {
+                    // Use a tab to indent each line of the file.
+                    available_ingredients[ingNo] = line;
+
+                    ingNo++;
+                }
+
+                string strProvider = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = CocktailsDB.accdb";
+                string strSql = "Select * from Cocktails";
+
+                OleDbConnection con = new OleDbConnection(strProvider);
+                OleDbCommand cmd = new OleDbCommand(strSql, con);
+                con.Open();
+                cmd.CommandType = CommandType.Text;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable scores = new DataTable();
+                da.Fill(scores);
+                dataGridView1.DataSource = scores;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // filter the unavailable cocktails if checkbox unchecked
+            if (!checkBox15.Checked)
+            {
+
+                for (int i = dataGridView1.Rows.Count - 1; i >= 0; i--)
+                {
+                    labelName.Text = Convert.ToString(dataGridView1.Rows.Count);
+                    bool display = true;
+                    string mix = Convert.ToString(dataGridView1.Rows[i].Cells["mix"].Value);
+                    string[] mix_split = mix.Split('|');
+
+                    foreach (string m_s in mix_split)
+                    {
+                        if (m_s != "" && !available_ingredients.Contains(m_s))
+                        {
+                            display = false;
+                        }
+                    }
+                    if (!display)
+                    {
+                        dataGridView1.Rows.RemoveAt(i);
+                    }
+                }
+            }
+            //hide unwanted columns
+            dataGridView1.Columns["ID"].Visible = false;
+            dataGridView1.Columns["desc"].Visible = false;
+            dataGridView1.Columns["img"].Visible = false;
+            dataGridView1.Columns["qty"].Visible = false;
+            dataGridView1.Columns["mix"].Visible = false;
+            dataGridView1.Columns["Name"].Width = 200;
+            dataGridView1.CurrentCell = dataGridView1[1, 0];
+            //dataGridView1_CellClick();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
